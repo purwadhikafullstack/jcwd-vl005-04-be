7a4af49f86539e3d9df1c0ec90d9383ccd21b7b7
Milzam Hutomo <milzam.hutomo@geniebook.com>
2022-07-04 08:23:08 +0700

    Create controller for Cart

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,11 +3,13 @@ const user = require('./user-controllers')
 const transaction = require('./transaction-controllers')
 const product = require('./product-controllers')
 const category = require('./category-controllers')
+const cart = require('./cart-controllers')
 
 module.exports = {
     admin, 
     user, 
     transaction,
     product,
-    category
+    category,
+    cart
 }
